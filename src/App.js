659299c45c3eb4439dbfc8dceca15c56e495c101
Tammanoon Jomjaturong <max@MaxBook-Pro-243900-Bath.local>
2019-06-05 11:57:12 +0700

    fix import bug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import './App.css';
 import options from './graphOptions'
-import Graph from './components/Graph'
 
 function App() {
   return (
