29c6bac5395ceecbfc7a6d53b11f527c876b4610
Tammanoon Jomjaturong <max@MaxBook-Pro-243900-Bath.local>
2019-06-05 11:56:18 +0700

    first commit with graph

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 import './App.css';
+import options from './graphOptions'
+import Graph from './components/Graph'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          {
+              options.map(item => (
+                  <HighchartsReact
+                      highcharts={Highcharts}
+                      options={item}
+                  />
+              ))
+          }
+
+      </div>
   );
 }
+
 
 export default App;
---
+++ src/graphOptions.js
@@ -0,0 +1,73 @@
+import { tcasData, khengData } from "./mockApi";
+
+const months = ['Jan','Feb','Mar','April','May','June','July','Aug','Sep','Oct','Nov','Dec']
+
+var options = [];
+
+options[0] = {
+    title: {
+        text: 'TCAS Goal'
+    },
+    xAxis: {
+        tickInterval: 1,
+        labels: {
+            enabled: true,
+            formatter: function() { return months[this.value];},
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Sales'
+        }
+    },
+    series:
+        Object.keys(tcasData).map(key => {
+            var value = tcasData[key];
+            var serieSum = 0;
+            return {
+                name: key,
+                data: value.map(val=>serieSum+=val)
+            };
+        })
+    ,
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    }
+}
+
+options[1] = {
+    title: {
+        text: 'Kheng Goal'
+    },
+    series:
+        Object.keys(khengData).map(key => {
+            var value = khengData[key];
+            var serieSum = 0;
+            return {
+                name: key,
+                data: value.map(val=>serieSum+=val)
+            };
+        })
+    ,
+    xAxis: {
+        tickInterval: 1,
+        labels: {
+            enabled: true,
+            formatter: function() { return months[this.value];},
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Sales'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    }
+}
+
+export default options;
