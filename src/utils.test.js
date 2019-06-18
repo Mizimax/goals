9ded6d51432c6823f46aa8f548d7d0d7c798dae7
max <tammanoon@dek-d.com>
2019-06-18 11:20:19 +0700

    test function toComma

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,14 +1,66 @@
 import { toComma } from './utils'
 
-it('sums numbers', () => {
-  expect(toComma(-1000)).toEqual('-1,000')
-  expect(toComma(-1)).toEqual('-1')
-  expect(toComma(0)).toEqual('0')
-  expect(toComma(100)).toEqual('100')
-  expect(toComma(1000)).toEqual('1,000')
-  expect(toComma('1000')).toEqual('1,000')
-  expect(toComma(1000000)).toEqual('1,000,000')
-  // it("Throw Error Invalid Data", () =>{
-  //   return expect(toComma({})).rejects.toEqual(new Error('Number, String format only'));
-  // })
+const specs = [
+  {
+    args: {
+      num: -1000,
+    },
+    result: '-1,000',
+  },
+  {
+    args: {
+      num: -1,
+    },
+    result: '-1',
+  },
+  {
+    args: {
+      num: 0,
+    },
+    result: '0',
+  },
+  {
+    args: {
+      num: 100,
+    },
+    result: '100',
+  },
+  {
+    args: {
+      num: 1000,
+    },
+    result: '1,000',
+  },
+  {
+    args: {
+      num: '1000',
+    },
+    result: '1,000',
+  },
+  {
+    args: {
+      num: 1000000,
+    },
+    result: '1,000,000',
+  },
+]
+
+const failSpec = {
+  args: {
+    num: {},
+  },
+  result: 'Number, String format only',
+}
+
+describe(`toComma(num) : num(Number,String)`, () => {
+  specs.forEach(({ args, result }) => {
+    it(`should return ${JSON.stringify(result)}, when passing props: ${JSON.stringify(
+      args.num
+    )}`, () => {
+      expect(toComma(args.num)).toBe(result)
+    })
+  })
+  it(`should throw Error(${failSpec.result}), when passing props: ${failSpec.args.num}`, () => {
+    expect(() => toComma(failSpec.args.num)).toThrow(failSpec.result)
+  })
 })
